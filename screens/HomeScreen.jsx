3a6c66ef40e9f2efb476460c30f4da6d1add77b4
shaheen7a <aousshaheen7@gmail.com>
2023-09-08 00:30:10 +0530

    Home Screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Alert,
+} from "react-native";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+        {/* bot icon */}
+        <View className="flex-row justify-center">
+          <Image
+            source={require("../assets/images/bot.png")}
+            style={{ height: hp(15), width: hp(15) }}
+          />
+        </View>
+      </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
